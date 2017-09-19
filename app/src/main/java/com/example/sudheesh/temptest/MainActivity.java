fcb6d338f73b40c9b2f87314eba6258004753ff1
ramdas <ramdasinfoinzenjer@gmail.com>
2017-09-19 11:27:26 +0530

    added progress bar

--- app/src/main/java/com/example/sudheesh/temptest/MainActivity.java
+++ app/src/main/java/com/example/sudheesh/temptest/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.sudheesh.temptest;
 
+import android.content.Context;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView web;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +24,30 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         web = (WebView) findViewById(R.id.inzenjer);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         WebSettings webSettings = web.getSettings();
         webSettings.setJavaScriptEnabled(true);
         web.loadUrl("http://www.inzenjer.somee.com");
-        web.setWebViewClient(new WebViewClient()); // to open links in webview client rather than external browser
+        web.setVisibility(View.INVISIBLE);
+        web.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE);
+                web.setVisibility(View.VISIBLE);
+            }
+        }); // to open links in webview client rather than external browser
     }
+
     //to nagivate to recent page when back button is pressed
     @Override
     public void onBackPressed() {
-        if(web.canGoBack()) {
+        if (web.canGoBack()) {
             web.goBack();
         } else {
             super.onBackPressed();
         }
     }
+
 
 }
